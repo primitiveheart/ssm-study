402635395aad07a98c36e3a1f801ea9467a39985
primitiveheart <1132924350@qq.com>
2018-03-03 18:08:09 +0800

    mybatis相关的配置

---
+++ doc/db.sql
@@ -0,0 +1,9 @@
+# 创建数据库
+CREATE database ssm_study;
+
+# 创建数据表
+CREATE TABLE user(
+   id INTEGER(5) PRIMARY KEY auto_increment,
+   name VARCHAR(10),
+   age INTEGER(3)
+);
---
+++ src/main/java/com/zgb/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.zgb.mapper;
+
+import com.zgb.entity.User;
+
+/**
+ * Created by admin on 2018/3/1.
+ */
+public interface UserMapper {
+    void insertUser(User user);
+}
